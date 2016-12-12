ffe8d7bec60b6b6e549a9b8c3c0c5e28893918e4
matt <matt@codeschool.com>
2016-12-12 16:12:04 -0500

    include vignette for working with FARS data

--- vignettes/fars_usage.R
+++ vignettes/fars_usage.R
@@ -1,4 +1,18 @@
-## ---- fig.show='hold'----------------------------------------------------
-plot(1:10)
-plot(10:1)
+## ---- message=FALSE------------------------------------------------------
+# devtools::install_github("madams1/courserapackage")
+require(courserapackage)
+require(dplyr)
+fars_2015_file <- system.file("extdata", make_filename(2015), package = "courserapackage")
+fars_path <- dirname(fars_2015_file)
+fars_2015_data <- fars_read(fars_2015_file)
+
+## ------------------------------------------------------------------------
+fars_2015_data
+
+## ------------------------------------------------------------------------
+fars_summarize_years(2013:2015, path = fars_path)
+
+## ---- fig.show='hold', message=FALSE-------------------------------------
+require(maps)
+fars_map_state(6, 2015, path = fars_path)
 
